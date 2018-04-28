6fc373df3779f5455fd4d7a62cca2729bfaf2c75
bellicose <www.renehr@gmail.com>
2018-04-28 09:14:16 -0500

    Se implementa el método agregar

--- src/main/java/com/bellicose/rest/service/FutbolistaREST.java
+++ src/main/java/com/bellicose/rest/service/FutbolistaREST.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -29,5 +30,15 @@ public class FutbolistaREST {
 			System.out.println(futbolista.toString());
 		}
 		return futbolistas;
+	}
+	
+	@POST
+	@Path("/agregar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Futbolista agregar(Futbolista futbolista){
+		FutbolistaDAO dao = new FutbolistaDAO();
+		dao.agregar(futbolista);
+		return futbolista;
 	}
 }
